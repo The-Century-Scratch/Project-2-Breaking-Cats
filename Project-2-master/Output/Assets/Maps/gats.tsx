cfcb4a589131d1b7aed01cda65f0b349ac684a21
SergioGarriguez <sergiogarriguez@gmail.com>
2023-03-26 19:30:42 +0200

    Adding Uri's template

---
+++ Project-2-master/Output/Assets/Maps/gats.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="gats" tilewidth="27" tileheight="28" tilecount="1" columns="1">
+ <image source="Gats.png" width="27" height="28"/>
+</tileset>
