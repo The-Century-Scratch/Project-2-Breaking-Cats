c59aaf86bb0241d521e6ec99d3fa65f1c6ca843c
Alex <101017492+faelion@users.noreply.github.com>
2023-04-17 04:41:10 +0200

    Vertical Sliced well

--- Game/Source/Map.cpp
+++ Game/Source/Map.cpp
@@ -132,9 +132,33 @@ bool Map::DrawObjectLayer(int index)
 
 void Map::DrawTileLayer(const MapLayer& layer) const
 {
-	for (int x = 0; x < layer.GetSize().x; x++)
+	auto camera = app->render->GetCamera();
+	camera.x *= -1;
+	camera.y *= -1;
+
+	iPoint cameraPosition = { camera.x, camera.y };
+	cameraPosition = WorldToMap(cameraPosition - tileSize);
+
+	iPoint cameraSize = { camera.w, camera.h };
+	cameraSize = WorldToMap(cameraSize);
+
+	SDL_Rect renderView{};
+
+	renderView.x = cameraPosition.x;
+	if (renderView.x < 0) renderView.x = 0;
+
+	renderView.w = cameraPosition.x + cameraSize.x + tileSize.x;
+	if (renderView.w > layer.GetSize().x) renderView.w = layer.GetSize().x;
+
+	renderView.y = cameraPosition.y;
+	if (renderView.y < 0) renderView.y = 0;
+
+	renderView.h = cameraPosition.y + cameraSize.y + tileSize.y;
+	if (renderView.h > layer.GetSize().y) renderView.h = layer.GetSize().y;
+
+	for (int x = renderView.x; x < renderView.x + renderView.w; x++)
 	{
-		for (int y = 0; y < layer.GetSize().y; y++)
+		for (int y = renderView.y; y < renderView.y + renderView.h; y++)
 		{
 			int gid = layer.GetTileGid(x, y);
 
